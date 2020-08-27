1384c8dcf9b518c1d1b35ca2f159d92fac949d16
antonialucianapires <antonialucianapires@gmail.com>
2020-08-26 21:27:33 -0300

    feat: criação do botão concluir

--- assets/listaTarefas.js
+++ assets/listaTarefas.js
@@ -3,22 +3,33 @@ const criarTarefa = (evento) => {
     evento.preventDefault()
 
     const lista = document.querySelector('[data-list]')
-    const input = document.querySelector('[data-form-input]');
-    const valor = input.value;
-    console.log(valor);
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
 
-    
-    const tarefa = document.createElement('li');
-    tarefa.classList.add('task');
-    const conteudo = `<p class="content">${valor}</p>`;
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+    const conteudo = `<p class="content">${valor}</p>`
 
     tarefa.innerHTML = conteudo;
-    lista.appendChild(tarefa);
 
-    input.value = " ";
+    tarefa.appendChild(BotaoConclui())
+    lista.appendChild(tarefa)
+    input.value = " "
 }
 
 
 const novaTarefa = document.querySelector('[data-form-button]');
 
 novaTarefa.addEventListener('click', criarTarefa);
+
+const BotaoConclui = () => {
+    const botaoConclui = document.createElement('button');
+
+    botaoConclui.classList.add('check-button');
+    botaoConclui.innerText = 'concluir';
+    botaoConclui.addEventListener('click', () => {
+        console.log("fui clicado");
+    })
+
+    return botaoConclui;
+}
